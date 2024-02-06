1467a73daa09be8b067531b82edc040a04c311c1
shitalkatre <134849794+shitallog@users.noreply.github.com>
2024-02-06 19:00:45 +0530

    Your commit message

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hello! I am shital katre.</span><br>I\'m a Full Stack Web Developer.<br>I\'m currently working as a Laravel Developer at a startup called Nextgen Techno Ventures Pvt Ltd . I'm the Web Team Lead here.<br>I am passionate about Web Development. I am a quick learner and a team worker that gets the job done within the deadline. Always ready to learn new technologies. I have strong command in Programming Languages with OOPS and DSA. <br><br><br>Email Id : Shitalkatre370@gmail.com<br>Mobile No : +91 9075698697<br>Address :Nagpur, India <br>Status : Unavailable"
+      ],
+      contentType: 'html'
+    });
+  }, 100);
